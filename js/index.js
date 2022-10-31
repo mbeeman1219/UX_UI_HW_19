d0ae2278ce3bd6555d75b6115842e94f77a122be
mbeeman1219 <105966788+mbeeman1219@users.noreply.github.com>
2022-10-31 00:11:26 -0700

    js added

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
 
 console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function(){
+    $("button").hover(function() {
+        $(this).css("background-color", "white");
+    }, function() {
+        $(this).css("background-color", "#134841");
+    });
+});
+
+$(document).ready(function(){
+    $("button").hover(function() {
+        $(this).css("color", "black");
+    }, function() {
+        $(this).css("color", "white");
+    });
+});
